87a9d1a0f962011a4c3f2d28f11dac0bb37b134a
unknown <albirosaldoalfredo12@gmail.com>
2022-09-19 13:29:40 +0200

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let menu = document.querySelector(".umasection")
+menu.style.transition = ".8s"
+
+document.querySelector(".menu").onclick = () =>{
+    menu.classList.add("active")
+
+}
+document.querySelector(".close").onclick = () =>{
+    menu.classList.remove("active")
+
+
+}
+onscroll = () =>{
+    menu.classList.remove("active")
+
+}
